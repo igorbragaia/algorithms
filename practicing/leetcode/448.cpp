2c6edf4dc33f92214adf4fb543502b1c26a0d922
Igor Bragaia <igor.bragaia@gmail.com>
2018-04-13 17:48:58 -0300

    organizing files

--- practicing/leetcode/448.cpp
+++ practicing/leetcode/448.cpp
@@ -4,7 +4,7 @@ public:
         vector<int> answer;
         for(int i = 0; i < nums.size(); i++)
             if(nums[abs(nums[i]) - 1] > 0)
-                nums[abs(nums[i]) - 1] = - nums[abs(nums[i]) - 1];
+                nums[abs(nums[i]) - 1] *= -1;
         for(int i = 0; i < nums.size(); i++)
             if(nums[i] > 0)
                 answer.push_back(i + 1);
