280a44635cb39f1c8bb64dbd82d9288a6dce604c
igorbragaia <igor.bragaia@gmail.com>
2019-07-03 18:23:05 -0300

    gauss-seidel and jacobi

---
+++ linear-algebra/jacobi.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+A = np.array([[5, 2, 1, 1], [2, 6, 2, 1], [1, 2, 7, 2], [1, 1, 2, 8]], dtype=np.float32)
+b = np.array([[29], [31], [26], [19]], dtype=np.float32)
+
+x_previous = np.array([[0], [0], [0], [0]], dtype=np.float32)
+
+rnorm = 1
+k = 0
+while rnorm > 1e-5:
+    k += 1
+    x = np.array([[0], [0], [0], [0]], dtype=np.float32)
+    for i in range(A.shape[0]):
+        x[i][0] = (1/A[i][i]) * (b[i][0] - sum([A[i][j] * x_previous[j][0] for j in range(A.shape[1]) if j != i]))
+
+    r = b - A.dot(x)
+    rnorm = np.linalg.norm(r)
+    print("ITERAÇÃO ", k)
+    print(x)
+    print("norma do erro r")
+    print(rnorm)
+    print("")
+
+    x_previous = x
