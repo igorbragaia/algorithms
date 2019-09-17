6dc49b4e424b24d5e36ca9bb249ed5901c17fc89
igorbragaia <igor.bragaia@gmail.com>
2019-09-17 03:06:19 -0300

    adding some good practices

--- random/n-queens.py
+++ random/n-queens.py
@@ -22,27 +22,29 @@ class Queen(object):
 
 class Queens(object):
     def __init__(self, n):
+        if n < 1:
+            raise Exception('let n be higher or equal to 1')
         possible_positions = []
         for i in range(n):
             for j in range(n):
                 possible_positions.append((i, j))
 
         self.n = n
+        self._queens = []
         self.possible_positions = possible_positions
-        self.done = set()
-        self._queens = self.positions(shuffle=True)
 
-    def positions(self, shuffle=False):
-        if not shuffle:
-            return self._queens
-        queens = [Queen(pos[0], pos[1]) for pos in random.sample(self.possible_positions, self.n)]
-        queens_hash = str(queens)
-        while queens_hash in self.done:
-            queens = self.positions(shuffle=True)
-            queens_hash = str(queens)
-        self.done.add(queens_hash)
-        self._queens = queens
+    def shuffle(self):
+        self._queens = [Queen(pos[0], pos[1]) for pos in random.sample(self.possible_positions, self.n)]
+
+    @property
+    def positions(self):
+        if not len(self._queens):
+            self.shuffle()
         return self._queens
+
+    @positions.setter
+    def positions(self, positions):
+        self._queens = positions
 
     def __str__(self):
         str_hash = ''
@@ -65,75 +67,57 @@ class NQueensIterativeSolver(ABC):
         pass
 
     @staticmethod
-    def hash(queens: list):
-        str_hash = ''
-        n = len(queens)
-        for i in range(n):
-            str_hash += " ".join(['x' if Queen(i, j) in queens else 'o' for j in range(n)]) + "\n"
-        return str_hash
-
-    @staticmethod
-    def all_possible_next_queens(queens, i):
+    def moves_list(queens: Queens, i: int):
+        positions = queens.positions
         response = []
-        n = len(queens)
-        moves = [(0, 1), (0, -1), (1, 0), (-1, 0), (1, -1), (1, 1), (-1, 1), (-1, -1)]
+        n = len(positions)
+        moves = [(1, -1), (1, 1), (-1, 1), (-1, -1)]
         for move in moves:
-            if 0 <= queens[i].x + move[0] < n and 0 <= queens[i].y + move[1] < n:
-                new_queen = Queen(queens[i].x + move[0], queens[i].y + move[1])
-                if new_queen not in queens:
-                    response.append([queens[j] if j != i else new_queen for j in range(n)])
+            if 0 <= positions[i].x + move[0] < n and 0 <= positions[i].y + move[1] < n:
+                new_queen = Queen(positions[i].x + move[0], positions[i].y + move[1])
+                if new_queen not in positions:
+                    new_queens = Queens(n)
+                    new_queens.positions = [positions[j] if j != i else new_queen for j in range(n)]
+                    response.append(new_queens)
+
         return response
 
     def solve(self, n):
-        if n < 1:
-            raise Exception('let n be higher than zero')
-        possible_positions = []
-        for i in range(n):
-            for j in range(n):
-                possible_positions.append((i, j))
-
-        done = set()
-        path = []
-        while not len(path):
-            path = []
-            queens = [Queen(pos[0], pos[1]) for pos in random.sample(possible_positions, n)]
-            queens_hash = self.hash(queens)
-            if queens_hash not in done:
-                done.add(queens_hash)
-                cost = 100*100
-                while cost:
-                    new_cost = self.cost(queens)
-                    next_queens = self.next(queens)
-                    if new_cost != cost:
-                        cost = new_cost
-                        path.append(next_queens)
-                        queens = next_queens
-                    else:
-                        queens = []
-                        path = []
-                        break
-                print(cost)
-        return path
+        while True:
+            queens = Queens(n)
+            cost = 100 * 100
+            while cost:
+                new_cost = self.cost(queens)
+                next_queens = self.next(queens)
+                if new_cost != cost:
+                    cost = new_cost
+                    queens = next_queens
+                else:
+                    break
+            print(cost)
+            if not cost:
+                return queens
 
 
 class HillClimbing(NQueensIterativeSolver):
     def __init__(self):
         super().__init__()
 
-    def cost(self, queens: list):
+    def cost(self, queens: Queens):
+        positions = queens.positions
         cost = 0
-        for i in range(len(queens)):
-            queen_cost = 0
-            for j in range(len(queens)):
-                queen_cost += queens[i].attacking(queens[j])
-            cost += queen_cost
+        for i in range(len(positions)):
+            positions_cost = 0
+            for j in range(len(positions)):
+                positions_cost += positions[i].attacking(positions[j])
+            cost += positions_cost
         return cost
 
-    def next(self, queens):
+    def next(self, queens: Queens):
         next_cost = self.cost(queens)
         next_queens = queens
-        for i in range(len(queens)):
-            for possible_next_queens in self.all_possible_next_queens(queens, i):
+        for i in range(len(queens.positions)):
+            for possible_next_queens in self.moves_list(queens, i):
                 possible_next_cost = self.cost(possible_next_queens)
                 if possible_next_cost < next_cost:
                     next_cost = possible_next_cost
@@ -142,13 +126,6 @@ class HillClimbing(NQueensIterativeSolver):
 
 
 if __name__ == '__main__':
-    queens = Queens(5)
-    print(str(queens))
-    # hillclimbing = HillClimbing()
-    # path = hillclimbing.solve(5)
-    # i = 1
-    # for positions in path:
-    #     print("iteracao {0}".format(i))
-    #     print(hillclimbing.hash(positions))
-    #     print()
-    #     i += 1
+    hillclimbing = HillClimbing()
+    queens = hillclimbing.solve(5)
+    print(queens)
